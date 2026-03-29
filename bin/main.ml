4fb78971f19b72447462098c3e5cd7c4401dfed2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-29 16:21:53 +0200

    sketch entry enum

--- bin/main.ml
+++ bin/main.ml
@@ -1,12 +1,38 @@
-[@@@warning "-69"]
+[@@@warning "-69-34-37"]
 
-type entry =
+type common_fields =
     {
         author: string;
         title: string;
         year: int;
         archive: string;
     }
+
+type month = int
+
+type article_fields =
+    {
+        journal: string;
+        volume: int;
+        pages: int*int;
+        number: int;
+        month: month;
+        doi: string;
+    }
+
+type inproceedings_fields =
+    {
+        booktitle: string;
+        pages: int*int;
+        doi: string;
+    }
+
+type misc_fields = (string * string) list
+
+type bib_entry =
+    | Article of common_fields * article_fields
+    | Inproceedings of common_fields * inproceedings_fields
+    | Misc of common_fields * misc_fields
 
 let foo =
     {
