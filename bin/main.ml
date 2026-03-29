4acf9c4c9a4774ec491d6ad64ace1bab9e3f5010
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-29 16:09:39 +0200

    sketch entry type

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,19 @@
-let () = print_endline "Hello, World!"
+[@@@warning "-69"]
+
+type entry =
+    {
+        author: string;
+        title: string;
+        year: int;
+        archive: string;
+    }
+
+let foo =
+    {
+        author = "hello";
+        title = "world";
+        year = 67;
+        archive = "google.com";
+    }
+
+let () = print_endline foo.author
