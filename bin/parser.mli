2858944250a84911eb4efbaf07c669bb4d5ed599
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-02 00:26:56 +0200

    refactor

--- bin/parser.mli
+++ bin/parser.mli
@@ -2,6 +2,6 @@ type t
 type error
 
 val init : string -> t
-val next_raw_entry : t -> t * (Entry.raw_entry, error) result
+val next_raw_entry : t -> t * (Entry.raw_entry option, error) result
 val show : t -> string
 val __test : unit -> unit
