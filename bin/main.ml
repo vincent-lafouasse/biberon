dd8dd43ba05b7499caa4ce0eee27919c5e61c59a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-02 23:36:35 +0200

    finally parse some bib

--- bin/main.ml
+++ bin/main.ml
@@ -10,19 +10,26 @@ let tiny_bib =
 @misc {an_entry,
 	a = "A",
 	b = 67,
-	c = true
+	c = true,
 }
 |}
 ;;
 
-let tokens = Lexer.tokenize tiny_bib
-let tokens = Result.get_ok tokens
-
 let print_tokens tokens =
-  Array.iter (fun (tok, _pos) -> Printf.printf "%s\n" (Token.show tok)) tokens
+  Array.iter
+    (fun (tok, pos) -> Printf.printf "%s @ %s\n" (Token.show tok) (Position.show pos))
+    tokens
 ;;
 
-let () = print_tokens tokens
+let print_entries =
+  Array.iter (fun entry -> Printf.printf "%s\n" (Entry.show_raw_entry entry))
+;;
 
-let () = Lexer.__test ()
-let () = Parser.__test ()
+let () =
+  let () = Printf.printf "%s\n" tiny_bib in
+  let maybe_entries = Parser.parse tiny_bib in
+  match maybe_entries with
+  | Error (err, loc) ->
+    die (Printf.sprintf "error at %s: %s" (Position.show loc) (Parser.show_error err))
+  | Ok entries -> print_entries entries
+;;
