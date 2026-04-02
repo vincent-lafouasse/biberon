3b5d222a91cdff211f5ed6a124d99b9f6c59845e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-02 23:36:28 +0200

    interface the parser module

--- bin/parser.mli
+++ bin/parser.mli
@@ -1,5 +1,8 @@
 type error
 
+val pp_error : Format.formatter -> error -> unit
+val show_error : error -> string
+
 val parse : string -> (Entry.raw_entry Array.t, error Position.located) result
 
 val __test : unit -> unit
