98981ae5990c3823dd4c6d50d80da81078a2b2b2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-02 09:00:25 +0200

    hand test

--- bin/main.ml
+++ bin/main.ml
@@ -5,5 +5,24 @@ let die msg =
   exit 1
 ;;
 
+let tiny_bib =
+  {|
+@misc {an_entry,
+	a = "A",
+	b = 67,
+	c = true
+}
+|}
+;;
+
+let tokens = Lexer.tokenize tiny_bib
+let tokens = Result.get_ok tokens
+
+let print_tokens tokens =
+  Array.iter (fun (tok, _pos) -> Printf.printf "%s\n" (Token.show tok)) tokens
+;;
+
+let () = print_tokens tokens
+
 let () = Lexer.__test ()
 let () = Parser.__test ()
