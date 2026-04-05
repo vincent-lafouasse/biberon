b32828b1c1254433607dc4dac319d2ac599d8432
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-05 07:22:57 +0200

    implement token repr

--- bin/token.ml
+++ bin/token.ml
@@ -4,6 +4,13 @@ module Value = struct
     | Integer of int
     | String of string
   [@@deriving show]
+
+  let format (value : t) : string =
+    match value with
+    | String s -> Printf.sprintf "string \"%s\"" s
+    | Integer i -> Printf.sprintf "integer '%d'" i
+    | Boolean b -> Printf.sprintf "boolean '%b'" b
+  ;;
 end
 
 type t =
@@ -16,3 +23,15 @@ type t =
   | Comma
   | Eof
 [@@deriving show]
+
+let format (token : t) : string =
+  match token with
+  | Identifier ident -> Printf.sprintf "identifier \"%s\"" ident
+  | Value value -> Value.format value
+  | EqualSign -> "'='"
+  | Lbrace -> "'{'"
+  | Rbrace -> "'}'"
+  | AtSign -> "'@'"
+  | Comma -> "','"
+  | Eof -> "end of file"
+;;
