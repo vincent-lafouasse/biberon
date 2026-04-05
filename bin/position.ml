cb33c0bcc11bd20fdf632c162fd25cb1f5beb59a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-05 06:51:56 +0200

    implement Position.report_error

--- bin/position.ml
+++ bin/position.ml
@@ -8,3 +8,13 @@ type t =
 type 'a located = 'a * t [@@deriving show]
 
 let distance pos_from pos_to = pos_to.absolute - pos_from.absolute
+
+let report_error (location : t) (input : string) (message : string) : string =
+  let lines : string list = Str.split (Str.regexp "\n") input in
+  let faulty_line = List.nth lines location.line in
+  let line_marker = string_of_int location.line ^ " | " in
+  let marked_faulty_line = line_marker ^ faulty_line in
+  let error_pointer_col : int = String.length line_marker + location.column in
+  let error_pointer = String.make error_pointer_col '~' ^ "^" in
+  marked_faulty_line ^ "\n" ^ error_pointer ^ "\n" ^ message
+;;
