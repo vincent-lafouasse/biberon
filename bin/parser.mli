f429ec4094f849ae99e93ed218c3d5012173ac39
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-05 07:05:45 +0200

    arrange Parser declarations

--- bin/parser.mli
+++ bin/parser.mli
@@ -1,8 +1,8 @@
 type error
 
+val parse : string -> (Entry.raw_entry Array.t, error Position.located) result
+
 val pp_error : Format.formatter -> error -> unit
 val show_error : error -> string
-
-val parse : string -> (Entry.raw_entry Array.t, error Position.located) result
 
 val __test : unit -> unit
