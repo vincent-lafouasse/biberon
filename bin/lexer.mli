370bc95b29e23665b4bc02bc0b6427c1d203ce32
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-05 07:05:23 +0200

    arrange Lexer declarations

--- bin/lexer.mli
+++ bin/lexer.mli
@@ -1,14 +1,13 @@
 type t
 type error
 
+val tokenize : string -> (Token.t Position.located array, error Position.located) result
+
 val pp : Format.formatter -> t -> unit
 val show : t -> string
 val pp_error : Format.formatter -> error -> unit
 val show_error : error -> string
-
 val format_error : error -> string
 val format_located_error : error Position.located -> string -> string
-
-val tokenize : string -> (Token.t Position.located array, error Position.located) result
 
 val __test : unit -> unit
