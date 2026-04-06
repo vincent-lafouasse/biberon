db62069586ae69d08f8dfa698199ee17c1431a83
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-06 22:27:44 +0200

    remove dead code

--- bin/main.ml
+++ bin/main.ml
@@ -19,32 +19,6 @@ let validate_or_die lib =
   | Error err -> die (Validate.format_full_error err)
 ;;
 
-let lamport1983 =
-  {|
-@article{Lamport1983,
-	author     = "Lamport, Leslie",
-	title      = "Specifying Concurrent Program Modules",
-	year       = 1983,
-
-	journal    = "{ACM} Trans. Program. Lang. Syst.",
-	volume     = 5,
-	number     = 2,
-	pages      = "190--222",
-	month      = "apr",
-
-	issue_date = "April 1983",
-	publisher  = "Association for Computing Machinery",
-	address    = "New York, NY, USA",
-	issn       = "0164-0925",
-	numpages   = "33",
-
-	doi        = "10.1145/69624.357207",
-	archive    = "https://lamport.azurewebsites.net/pubs/spec.pdf",
-	note       = "foundational proof of correctness of SPSC queues",
-}
-|}
-;;
-
 let () =
   let path =
     match Array.length Sys.argv with
